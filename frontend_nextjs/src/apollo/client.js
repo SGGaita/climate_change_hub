ab97cf13965279c16e200aa32e192c44ea204e88
Steve Gaita <steveggaita@gmail.com>
2024-09-08 15:35:01 +0300

    apollo setup

--- frontend_nextjs/src/apollo/client.js
+++ frontend_nextjs/src/apollo/client.js
@@ -1,6 +1,56 @@
-import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider, gql } from '@apollo/client';
+"use client";
 
-const client = new ApolloClient({
-    uri: 'https://flyby-router-demo.herokuapp.com/',
-    cache: new InMemoryCache(),
+import { createHttpLink, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, ApolloNextAppProvider, InMemoryCache, } from '@apollo/experimental-nextjs-app-support';
+
+
+
+
+const makeClient = () => {
+  //configuration for different GraphQL operation types
+  const defaultOptions = {
+    //queries that need to stay up-to-date with real-time server changes.
+    watchQuery: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
+
+    },
+    //one-time data fetching
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "all",
+    }
+  }
+
+  //Apollo Client won't store query results in memory by default
+  const cache = new InMemoryCache({
+    resultCaching: false,
+  })
+
+  // creates a link to your GraphQL server
+  const link = createHttpLink({
+    uri: `${process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL}/graphql`,
+  })
+
+  console.warn("URL", process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL)
+
+  return new ApolloClient({
+    link,
+    cache,
+    defaultOptions
   });
+
+
+}
+
+
+// you need to create a component to wrap your app in
+const ApolloWrapper =({ children }) =>{
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
+
+export default ApolloWrapper;
